f223e9098a294d2f7728ca960f29d68a1f9d1fd3
Rajesh Kumar Yadav <rajeshraj8991@gmail.com>
2021-05-24 18:50:20 +0530

    sumtion

---
+++ springBootStopWatch/src/main/java/com/rajesh/SpringBootStopWatchApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStopWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStopWatchApplication.class, args);
+	}
+
+}
